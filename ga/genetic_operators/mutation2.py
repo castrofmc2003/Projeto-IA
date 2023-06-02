4c991429552ace52d966a9de54871ca8f9a70109
Francisco Castro <2211035@my.ipleiria.pt>
2023-06-02 19:22:24 +0100

    Recombination3 mutation2

--- ga/genetic_operators/mutation2.py
+++ ga/genetic_operators/mutation2.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from ga.individual_int_vector import IntVectorIndividual
 from ga.genetic_operators.mutation import Mutation
 
@@ -6,8 +8,25 @@ class Mutation2(Mutation):
         super().__init__(probability)
 
     def mutate(self, ind: IntVectorIndividual) -> None:
-        # TODO
-        pass
+        # Scramble mutation
+        num_genes = len(ind.genome)
+        cut1 = np.random.randint(0, num_genes - 1)
+        cut2 = cut1
+        while (cut1 == cut2):
+            cut2 = np.random.randint(0, num_genes - 1)
+
+        if cut1 > cut2:
+            cut1, cut2 = cut2, cut1
+        aux = []
+        index = 0
+
+        for i in range(cut1, cut2):
+            aux.append(ind.genome[i])
+        np.random.shuffle(aux)
+
+        for i in range(cut1, cut2):
+            ind.genome[i] = aux[index]
+            index +=1
 
     def __str__(self):
         return "Mutation 2 (" + f'{self.probability}' + ")"
--- ga/genetic_operators/recombination3.py
+++ ga/genetic_operators/recombination3.py
@@ -1,3 +1,6 @@
+
+import numpy as np
+
 from ga.genetic_algorithm import GeneticAlgorithm
 from ga.genetic_operators.recombination import Recombination
 from ga.individual import Individual
@@ -7,9 +10,40 @@ class Recombination3(Recombination):
         super().__init__(probability)
 
     def recombine(self, ind1: Individual, ind2: Individual) -> None:
-        for i in range(ind1.num_genes):
-            if GeneticAlgorithm.rand.random() < self.probability:
-                ind1.genome[i], ind2.genome[i] = ind2.genome[i], ind1.genome[i]
+        # Cycle Crossover
+        size = len(ind1.genome)
+        offspring1 = [-1] * size
+        offspring2 = [-1] * size
+
+        # Select a random index to start the cycle
+        index = np.random.randint(0, size - 1)
+        indexInic = index
+        # Add the first element to the offspring
+        offspring1[index] = ind1.genome[index]
+        offspring2[index] = ind2.genome[index]
+        while True:
+            # Add the current element to the offspring
+
+            # Find the element in parent2 that is at the same index as the element in parent1
+            value = ind2.genome[index]
+            for i in range(size):
+                if ind1.genome[i] == value:
+                    index = i
+                    break
+            offspring1[index] = ind1.genome[index]
+            offspring2[index] = ind2.genome[index]
+            # Check if the cycle is complete
+            if ind2.genome[index] == ind1.genome[indexInic]:
+                break
+
+        # Fill in the remaining elements in the offspring
+        for i in range(size):
+            if offspring1[i] == -1:
+                offspring1[i] = ind2.genome[i]
+                offspring2[i] = ind1.genome[i]
+        ind2.genome = offspring2
+        ind1.genome = offspring1
+        return ind1.genome, ind2.genome
 
     def __str__(self):
         return "Recombination 3 (" + f'{self.probability}' + ")"
