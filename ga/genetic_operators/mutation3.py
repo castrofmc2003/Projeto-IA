b2570d1367325cf164079df4b213a6e97debeaa4
Francisco Castro <2211035@my.ipleiria.pt>
2023-06-03 18:30:25 +0100

    mutation3

--- ga/genetic_operators/mutation3.py
+++ ga/genetic_operators/mutation3.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from ga.individual_int_vector import IntVectorIndividual
 from ga.genetic_operators.mutation import Mutation
 
@@ -6,8 +8,14 @@ class Mutation3(Mutation):
         super().__init__(probability)
 
     def mutate(self, ind: IntVectorIndividual) -> None:
-        # TODO
-        pass
+        num_genes = len(ind.genome)
+        cut1 = np.random.randint(0, num_genes - 1)
+        cut2 = np.random.randint(cut1 + 1, num_genes)
+
+        aux = ind.genome[cut1 + 1]
+        ind.genome[cut1 + 1] = ind.genome[cut2]
+        for i in range(cut1 + 2, cut2 + 1):
+            ind.genome[i], aux = aux, ind.genome[i]
 
     def __str__(self):
         return "Mutation 3 (" + f'{self.probability}' + ")"
